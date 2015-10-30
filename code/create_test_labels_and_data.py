2eefd9e29afb663462cde2eefc4a299562e826b1
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-10-30 20:44:41 +0100

    Fix in creation of test data

--- code/create_test_labels_and_data.py
+++ code/create_test_labels_and_data.py
@@ -1,5 +1,10 @@
 __author__ = 'Lukas Woodtli'
 
+import os
+
+WORKING_DIR = os.path.dirname(__file__)
+IN_FILE = os.path.join(WORKING_DIR, "..", "data", "training.txt")
+
 # run this only once
 with open(IN_FILE) as training_f:
     with open(os.path.join(WORKING_DIR, "test_data.txt"), 'w') as test_data_f:
