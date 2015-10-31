a20010b480cc4cef1620f779f4275fccd3ea6bf0
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-10-31 22:49:22 +0100

    Try to implement AdaGrad

--- code/mapper.py
+++ code/mapper.py
@@ -3,22 +3,25 @@
 
 import sys
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 
 
 DIMENSION = 400  # Dimension of the original data.
 CLASSES = (-1, +1)   # The classes that we are trying to predict.
 
 #ETHA = 0.005
-ETHA = 0.005
+ETHA = 0.1
 
 #C = 0.001
-C = 0.001
 
-transform_param = 1
+
+G_diag = np.full(DIMENSION + 1, 0.0, dtype=np.float128)
 
 def transform(x_original):
-    x_transformed = np.append(x_original, 0.0)
-    x_transformed = x_transformed / np.linalg.norm(x_transformed)
+    x_transformed = np.append(x_original, 0.5)
+    #x_transformed = x_transformed / np.linalg.norm(x_transformed)
+    x_transformed  = scaler.fit_transform(x_transformed)
     assert x_transformed.size == 401
     return x_transformed
 
@@ -33,10 +36,20 @@ def dL_dw(xi, yi, w, j):
 def gradient(x,y,w):
     assert w.size == 401
     assert x.size == 401
-    w_new = []
+    global G
+
     for j, wj in enumerate(w):
-        w_new.append(w[j] + C * dL_dw(x, y, w, j))
-    return np.array(w_new)
+        g_t = dL_dw(x, y, w, j)
+        G_diag[j] = G_diag[j] + g_t**2
+
+    assert  G_diag.size == 401
+
+    result = np.diagflat(G_diag)
+    result = np.sqrt(result)
+    result = np.divide(ETHA, result)
+    result = np.diag(result)
+    assert result.size == 401
+    return w - result * g_t
 
 
 def main(stream):
@@ -45,16 +58,22 @@ def main(stream):
     assert w.size == 401
 
 
-    for line in stream[0:10000]:
+    for line in stream:
         line = line.strip()
         (label, x_string) = line.split(" ", 1)
         label = int(label)
         x_original = np.fromstring(x_string, sep=' ')
         x = transform(x_original)  # Use our features.
 
-        w = w - ETHA * gradient(x, label, w)
+        grad = gradient(x, label, w)
+        w_old = w
+        w = w - grad
+        if np.linalg.norm(w_old - w) < 0.001: break
+
+
         assert w.size == 401
 
+    print w
     assert w.size == 401
     result = ""
     for i in w:
