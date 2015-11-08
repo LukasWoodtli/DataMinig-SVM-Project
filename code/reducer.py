01b751c4defc2c1319022a1d066a966afdb2cc40
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-11-08 20:04:35 +0100

    Reducer angepasst

--- code/reducer.py
+++ code/reducer.py
@@ -5,7 +5,26 @@ import logging
 import sys
 import numpy as np
 
-if __name__ == "__main__":
-    for line in sys.stdin:
+def main(stream):
+    number_of_elemts = 0
+    w_new = None
+    for line in stream: #.splitlines():
         line = line.strip()
-    
+        line = np.fromstring(line, sep=' ')
+        if w_new:
+            w_new = np.add(w_new, line)
+        else:
+            w_new = line
+
+        number_of_elemts += 1
+
+    w = np.divide(w_new, number_of_elemts)
+
+    result = ""
+    for i in w:
+        result += "%f " % i
+    return result
+
+if __name__ == "__main__":
+    print main(sys.stdin)
+
